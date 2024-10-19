74ece0e1d8ca57d7f5623c22a24ea01842bb9958
ruzakiff <ryanchenyang@gmail.com>
2024-10-19 12:07:04 -0400

    removedpickle

--- frontend/backend.py
+++ frontend/backend.py
@@ -96,5 +96,5 @@ def get_predictions():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.logger.info('Application starting')
+    app.run(host='0.0.0.0', port=5000, debug=False)
